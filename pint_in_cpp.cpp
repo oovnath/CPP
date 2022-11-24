1b5afeab5c0eec5a47f477d07f08b48039179ea5
Abhishek Nath <68547909+oovnath@users.noreply.github.com>
2022-11-24 05:20:09 +0000

    print function

---
+++ pint_in_cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iostream>
+using namespace std ;
+// Just to avoid using std every time using cout , cin
+
+ int main()
+ {
+    cout<<"Hello World" ;
+    return 0;
+    
+ }
+ 
